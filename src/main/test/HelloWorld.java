8e05634f53179d1b12962c9f1f79908a343ff30e
qishuiqing <1121062667@qq.com>
2017-12-16 19:08:03 +0800

    Main class

---
+++ src/main/test/HelloWorld.java
@@ -0,0 +1,8 @@
+package main.test;
+
+public class HelloWorld {
+    public static void main(String ags[]){
+        System.out.println("you are SB");
+        System.out.println("nadada");
+    }
+}
--- src/main/test/helloWorld.java
+++
@@ -1,7 +0,0 @@
-package main.test;
-
-public class helloWorld {
-    public void main(String ags[]){
-        System.out.println("you are SB");
-    }
-}
