bde16564e0b9ad2dd926afa4187676a2c49f9106
qishuiqing <1121062667@qq.com>
2017-12-21 23:16:14 +0800

    ajust

--- src/main/test/HelloWorld.java
+++ src/main/test/HelloWorld.java
@@ -3,6 +3,7 @@ package main.test;
 public class HelloWorld {
     public static void main(String ags[]){
         System.out.println("you are SB");
+		
         System.out.println("nadada");
     }
 }
